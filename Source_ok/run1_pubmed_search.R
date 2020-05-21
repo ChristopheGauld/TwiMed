43a2263a7a3e3eeda5f75dd6c503ac6b06fa8ca4
ChristopheGauld <54821696+ChristopheGauld@users.noreply.github.com>
2020-05-21 20:57:50 +0200

    Update run1_pubmed_search.R

--- Source_ok/run1_pubmed_search.R
+++ Source_ok/run1_pubmed_search.R
@@ -10,7 +10,7 @@ output_file <- "data/pubmed.Rdata"
 # pubmed extraction
 search_query <- ("autis*")
 res <- pmQueryTotalCount(search_query)
-search_output <- pmApiRequest(query = search_query, # limit = 1000 #res$total_count
+search_output <- pmApiRequest(query = search_query,res$total_count # limit = 1000 
                               , api_key = NULL)
 
 # convert to a dataframe
