77e40f16f638417e692e5589c436c855d8083b90
ChristopheGauld <54821696+ChristopheGauld@users.noreply.github.com>
2020-05-31 15:16:34 +0200

    Update run2_pubmed_text_mining.R

--- src/run2_pubmed_text_mining.R
+++ src/run2_pubmed_text_mining.R
@@ -1,3 +1,14 @@
+#!/usr/bin/env Rscript
+# coding=utf-8
+# ==============================================================================
+# description     : processing pipeline to create matrix
+# date            : 2020-05-12
+# version         : 2
+# ==============================================================================
+
+
+
+
 rm(list=ls())
 
 library(tidytext)
