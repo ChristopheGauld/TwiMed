9df2bfe040a3260df10ca71bfa294e6e8adfd366
JulienMaquet <65130088+JulienMaquet@users.noreply.github.com>
2020-06-04 17:48:22 +0200

    Update run6_twitter_matrix_to_bootstrap.R

--- src/run6_twitter_matrix_to_bootstrap.R
+++ src/run6_twitter_matrix_to_bootstrap.R
@@ -1,44 +1,46 @@
- #!/usr/bin/env Rscript
+#!/usr/bin/env Rscript
 # coding=utf-8
 # ==============================================================================
 # description     : processing pipeline for bootstrat Twitter data
-# date            : 2020-05-12
-# version         : 2
+# date            : 2020-06-04
+# version         : 4
 # ==============================================================================
 
-
-#### Use run_twitter_02 to obtain the matrix tidy_hastag_4
-
-# Save the 15 most frequent words
-tidy_hashtag4 <- count(tidy_hashtag3,word,sort=TRUE)
-words15 = tidy_hashtag4$word[1:15]
-
-# Bootsrap des occurences et raincloud plot #####
+input_file <- "../data/twitter_tdm.Rdata"
+output_file <- "../fig/fig1.pdf"
 
 library(boot)
-# Je définis la fonction occurence qui retourne le nombre d’occurence d’un word dans le vecteur data. Je laisse la ligne d =  data[indices] qui semble être nécessaire pour sélectionner les échantillons de data avec remise.
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+#### load the tidy objet of twitter words
+load(input_file)
+
+words15 <- tidy_twitter5$word[1:15]
+
+# Occurences Bootstrap and raincloud plot
+# Define the occurence function which returns the number of occurrences of a word in the vector data. Leave the line of data (indices seems to be necessary to select the samples of data with discount)
 occurence_random_tweet <- function(data, indices, word) {
   autis_tweets <- data[indices,] # allows boot to select sample
-  tidy_tweeter <- autis_tweets %>%
-    unnest_tokens(word, text, token="tweets", strip_url=TRUE)
-  return(sum(tidy_tweeter$word == word))
+  tidy_twitter <- autis_tweets %>%
+    unnest_tokens(word, text)
+  return(sum(tidy_twitter$word == word))
 }
+
 # initialisation d'un tableau de résultat vide
-results=matrix(NA,1000,15)
-results=as.data.frame(results)
-# Pour chacun des 15 mots, on calcule les 1000 valeurs de bootsrap et on les mets dans une colonne du tableau de résultats
+results <- matrix(NA,1000,15)
+results <- as.data.frame(results)
+# For each of the 15 words, we calculate the 1000 values of bootstrap and put them in a column of the results table
 for (i in 1:15) {
   result <- boot(data=autis_tweets, statistic=occurence_random_tweet, R=1000, word=words15[i])
-  results[,i]=result$t # On remplit une colonne du dataframe des résultats avec les 1000 valeurs de bootsrap
-  names(results)[i]=words15[i] # On donne le mot en nom de variable du dataframe
-  print(names(results)[i])
+  results[,i] <- result$t # We fill a column of the dataframe of the results with the 1000 values of bootsrap
+  names(results)[i] <- words15[i] 
+print(names(results)[i])
 }
-summary(results[,1])
-quantile(results[,1],c(.025,.975)) #IC bootsrap
-PropCIs::exactci(281,15378,.95)$conf.int*15378 #IC méthode exacte de Clopper et Pearson (en piochant au hasard dans les 15378 hashtag et non dans tweets)
 
-# Boostrap en tirant au sort les mots et non les tweets :
-# Je définis la fonction occurence qui retourne le nombre d’occurence d’un word dans le vecteur data. Je laisse la ligne d =  data[indices] qui semble être nécessaire pour sélectionner les échantillons de data avec remise.
+# Bootstrap by drawing words, not tweets :
+# Define the occurence function which returns the number of occurrences of a word in the vector data. Leave the line of data (indices seems to be necessary to select the samples of data with discount)
 #occurence_random_word <- function(data, indices, word) {
 #  words <- data[indices] # allows boot to select sample
 #  return(sum(words == word))
@@ -57,16 +59,12 @@ PropCIs::exactci(281,15378,.95)$conf.int*15378 #IC méthode exacte de Clopper et
 #quantile(results2[,1],c(.025,.975))
 #PropCIs::exactci(281,15378,.95)$conf.int*15378
 
-
 ## Reformat the data for ggplot
-library(dplyr)
-library(tidyr)
-plotData <- gather(results2,hashtag,occurency
-                   ,factor_key = TRUE) #sert à avoir un plot ordonné ensuite
+plotData <- gather(results,wordname,occurency
+                   ,factor_key = TRUE) #to have a sort plot
 
 ## And plot the data
-library(ggplot2)
-source("/geom_flat_violin.R") # cf run_twitter_03b
+source("../src/run5_twitter_load_function_violin.R") 
 p <- ggplot(plotData,aes(x=plotData[,1],y=plotData[,2], 
                           fill = plotData[,1], colour = plotData[,1]),trim = TRUE)+
   geom_flat_violin(position = position_nudge(x = .3, y = 0), adjust = 1)+
