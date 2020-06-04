1dd9d8fe21ca47bb3eda24ca8a04f4c4011ba58c
JulienMaquet <65130088+JulienMaquet@users.noreply.github.com>
2020-06-04 17:22:28 +0200

    Update run4_pubmed_qgraph.R

--- src/run4_pubmed_qgraph.R
+++ src/run4_pubmed_qgraph.R
@@ -6,11 +6,8 @@
 # version         : 3
 # ==============================================================================
 
-
-
 rm(list=ls())
 
-setwd("~/Documents/Amis/Christophe/Twimed")
 input_file <- "data/pubmed_tdm_group.Rdata"
 output_file <- "data/pubmed_qgraph.Rdata"
 
@@ -19,18 +16,17 @@ library(qgraph)
 # load the pubmed tidy object and the terms order by group after topic modeling
 load(input_file)
 
-# Pour réduire la taille de la matrice à 300*300 au lieu de 13000*13000 pour pouvoir plotter en prenant les 300 mots les plus fréquents
-# Exécuter prog1 d'abord pour avoir l'objet tidy.pubmed2
-#nbMots <- 300
-#freq_word <- dplyr::top_n(dplyr::count(tidy.pubmed2, word),nbMots, n)
+# 
+nNode <- 300
+freq_word <- dplyr::top_n(dplyr::count(tidy.pubmed2, word),nNode, n)
 
-#matrix_reduite <- matrix_pubmed[,freq_word$word]
-#cor_matrix_reduite <- cor(matrix_reduite)
-#group1_reduit <- which(colnames(cor_matrix_reduite) %in% group[[1]]) 
-#group2_reduit <- which(colnames(cor_matrix_reduite) %in% group[[2]]) 
-#group3_reduit <- which(colnames(cor_matrix_reduite) %in% group[[3]]) 
-#group4_reduit <- which(colnames(cor_matrix_reduite) %in% group[[4]]) 
-#group_matrix_reduite <- list(group1_reduit,group2_reduit,group3_reduit,group4_reduit)
+matrix_reduite <- matrix_pubmed[,freq_word$word]
+cor_matrix_reduite <- cor(matrix_reduite)
+group1_reduit <- which(colnames(cor_matrix_reduite) %in% group[[1]]) 
+group2_reduit <- which(colnames(cor_matrix_reduite) %in% group[[2]]) 
+group3_reduit <- which(colnames(cor_matrix_reduite) %in% group[[3]]) 
+group4_reduit <- which(colnames(cor_matrix_reduite) %in% group[[4]]) 
+group_matrix_reduite <- list(group1_reduit,group2_reduit,group3_reduit,group4_reduit)
 
 # Définition de la matrice de corrélation de taille normale
 cor_matrix <- cor(matrix_pubmed)
