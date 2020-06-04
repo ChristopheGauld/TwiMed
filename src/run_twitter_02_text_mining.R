37a70fbae0bf4d1dcecb7cc4e240e4cc23442f86
ChristopheGauld <54821696+ChristopheGauld@users.noreply.github.com>
2020-06-04 17:02:56 +0200

    Update run_twitter_02_text_mining.R

--- src/run_twitter_02_text_mining.R
+++ src/run_twitter_02_text_mining.R
@@ -37,5 +37,12 @@ tidy_twitter2 <- words %>%
 tidy_twitter3 <- tidy_tweeter2[!str_detect(tidy_twitter2$word,"autis"),]
 tidy_twitter4 <- tidy_tweeter3[!str_detect(tidy_twitter3$word,c("1","2","3","4","5","6","7","8","9"))]
 
+# count frequency of each word 
+tidy_twitter5 <- dplyr::count(tidy_twitter4, word,sort=TRUE)
+
+# convert to a dtm
+dtm_twitter <- cast_dtm(tidy_twitter5, word, id, n)
+
 # save
-save(tidy_twitter4,output_file)
+save(dtm_twitter, file = output_file)
+
