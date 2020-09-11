12789b4b08937f4eadba812b4411d48b5ffc15be
Guillaume Dumas <deep@introspection.eu>
2020-09-11 16:58:09 +0200

    Correct Bibliometrix bug with 2df

--- src/run2_pubmed_text_mining.R
+++ src/run2_pubmed_text_mining.R
@@ -18,17 +18,18 @@ output_file <- "data/pubmed_tdm.Rdata"
 load(input_file)
 
 # select abstracts and PMID
-ab.pubmed <- results_pubmed[,c("PMID","AB")]
+ab.pubmed <- results_pubmed[, c("PMID","AB")]
 
 # split abstracts into words
-tidy.pubmed <- unnest_tokens(ab.pubmed,word, AB)
+tidy.pubmed <- unnest_tokens(ab.pubmed, word, AB)
 
 # remove common words and words containing "autism" or "disorder"
 data("stop_words")
 stop_words = rbind(stop_words,"autism","asd", "1", "2", "3", "4", "5", "6", "7", "8", "9")
-tidy.pubmed2 <- dplyr::anti_join(tidy.pubmed,stop_words)
-tidy.pubmed2 <- tidy.pubmed2[!str_detect(tidy.pubmed2$word,"autis"),]
-tidy.pubmed2 <- tidy.pubmed2[!str_detect(tidy.pubmed2$word,"disorder"),]
+class(tidy.pubmed) <- c("bibliometrixDB", "data.frame")
+tidy.pubmed2 <- dplyr::anti_join(tidy.pubmed, stop_words)
+tidy.pubmed2 <- tidy.pubmed2[!str_detect(tidy.pubmed2$word, "autis"),]
+tidy.pubmed2 <- tidy.pubmed2[!str_detect(tidy.pubmed2$word, "disorder"),]
 
 # count frequency of each word in each abstract
 tidy.pubmed3 <- dplyr::count(tidy.pubmed2, PMID, word)
