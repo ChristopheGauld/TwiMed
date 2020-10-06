7fd9a71df05f11c055c5d40926de54dc78c7617a
ChristopheGauld <54821696+ChristopheGauld@users.noreply.github.com>
2020-10-06 23:53:23 +0200

    Update run2_pubmed_text_mining.R

--- src/run2_pubmed_text_mining.R
+++ src/run2_pubmed_text_mining.R
@@ -23,7 +23,7 @@ ab.pubmed <- results_pubmed[, c("PMID","AB")]
 # split abstracts into words
 tidy.pubmed <- unnest_tokens(ab.pubmed, word, AB)
 
-# remove common words and words containing "autism" or "disorder"
+# remove common words and words containing "autism" or  "disorder"
 data("stop_words")
 stop_words = rbind(stop_words,"autism","asd", "1", "2", "3", "4", "5", "6", "7", "8", "9")
 class(tidy.pubmed) <- c("bibliometrixDB", "data.frame")
