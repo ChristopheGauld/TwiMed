e182e3e2d2dff12c4427334702acdbd10a490e38
ChristopheGauld <54821696+ChristopheGauld@users.noreply.github.com>
2020-10-07 00:09:43 +0200

    Update run4_pubmed_qgraph.R

--- src/run4_pubmed_qgraph.R
+++ src/run4_pubmed_qgraph.R
@@ -17,7 +17,8 @@ library(qgraph)
 load(input_file)
 
 # definition de la matrice réduite à 50 noeuds
-nNode <- 300
+nNode <- 50
+#nNode <- 500
 freq_word <- dplyr::top_n(dplyr::count(tidy.pubmed2, word),nNode, n)
 
 matrix_reduite <- matrix_pubmed[,freq_word$word]
@@ -26,6 +27,7 @@ group1_reduit <- which(colnames(cor_matrix_reduite) %in% group[[1]])
 group2_reduit <- which(colnames(cor_matrix_reduite) %in% group[[2]]) 
 group3_reduit <- which(colnames(cor_matrix_reduite) %in% group[[3]]) 
 group4_reduit <- which(colnames(cor_matrix_reduite) %in% group[[4]]) 
+group5_reduit <- which(colnames(cor_matrix_reduite) %in% group[[5]]) 
 group6_reduit <- which(colnames(cor_matrix_reduite) %in% group[[6]]) 
 group7_reduit <- which(colnames(cor_matrix_reduite) %in% group[[7]]) 
 group_matrix_reduite <- list(group1_reduit,group2_reduit,group3_reduit,group4_reduit,group5_reduit,group6_reduit,group7_reduit)
@@ -37,11 +39,16 @@ group_matrix_reduite <- list(group1_reduit,group2_reduit,group3_reduit,group4_re
 Q <- qgraph(cor_matrix_reduite, layout = "spring", posCol = "blue", negCol = "NA",
             nodeNames = colnames(cor_matrix_reduite), legend.cex = 0.2,
             groups = group_matrix_reduite,
-            label.scale = FALSE,
-            minimum = 0.005, # Ne pas afficher les edges avec faible corrélation pour faciliter le chargement graphique
+            label.scale = TRUE,
+            labels = TRUE,
+            minimum = 0.1, # 0.15 quand 500 noeuds
             #repulsion = 100, # Augmenter la distance entre les noeuds pour améliorer la visualisation en "cluster"
-            legend.mode = "groupe",sampleSize = nrow(cor_matrix_reduite), alpha = 0.05, # Pour ne prendre en compte que les corrélations statistiquement significatifs, en tenant compte de l'inflation du risque alpha par les tests multiples via la méthode de Bonferroni.
-            palette = "pastel", vsize = 2)
+            legend.mode = "style2", # groupe quand 500 noeuds
+            nodeNames= TRUE,
+            sampleSize = nrow(cor_matrix_reduite),
+            alpha = 0.05, # Pour ne prendre en compte que les corrélations statistiquement significatifs, en tenant compte de l'inflation du risque alpha par les tests multiples via la méthode de Bonferroni.
+            palette = "pastel",
+            vsize = 6)
 
 #labels=TRUE)
 #color = color,
