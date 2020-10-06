ad6a8993f0688cd52588926099c3b47d0c62c41c
ChristopheGauld <54821696+ChristopheGauld@users.noreply.github.com>
2020-10-06 23:51:19 +0200

    Update run4_pubmed_qgraph.R

--- src/run4_pubmed_qgraph.R
+++ src/run4_pubmed_qgraph.R
@@ -16,7 +16,7 @@ library(qgraph)
 # load the pubmed tidy object and the terms order by group after topic modeling
 load(input_file)
 
-# 
+# definition de la matrice réduite à 50 noeuds
 nNode <- 300
 freq_word <- dplyr::top_n(dplyr::count(tidy.pubmed2, word),nNode, n)
 
@@ -26,24 +26,26 @@ group1_reduit <- which(colnames(cor_matrix_reduite) %in% group[[1]])
 group2_reduit <- which(colnames(cor_matrix_reduite) %in% group[[2]]) 
 group3_reduit <- which(colnames(cor_matrix_reduite) %in% group[[3]]) 
 group4_reduit <- which(colnames(cor_matrix_reduite) %in% group[[4]]) 
-group_matrix_reduite <- list(group1_reduit,group2_reduit,group3_reduit,group4_reduit)
+group6_reduit <- which(colnames(cor_matrix_reduite) %in% group[[6]]) 
+group7_reduit <- which(colnames(cor_matrix_reduite) %in% group[[7]]) 
+group_matrix_reduite <- list(group1_reduit,group2_reduit,group3_reduit,group4_reduit,group5_reduit,group6_reduit,group7_reduit)
 
 # Définition de la matrice de corrélation de taille normale
-cor_matrix <- cor(matrix_pubmed)
+#cor_matrix <- cor(matrix_pubmed)
 
 # create a qgraph object
-Q <- qgraph(cor_matrix, layout = "spring", posCol = "blue", negCol = "red",
-           nodeNames = colnames(cor_matrix), legend.cex = 0.2,
-           groups = group,
-           #minimum = .15, # Ne pas afficher les edges avec faible corrélation pour faciliter le chargement graphique
-           #repulsion = 100, # Augmenter la distance entre les noeuds pour améliorer la visualisation en "cluster"
-           legend.mode = "groups",
-           threshold = "bonferroni",sampleSize = nrow(cor_matrix_reduite), alpha = 0.05) # Pour ne prendre en compte que les corrélations statistiquement significatifs, en tenant compte de l'inflation du risque alpha par les tests multiples via la méthode de Bonferroni.
-           #pastel = TRUE,
-           #labels=TRUE)
-           #vsize = taille,
-           #color = color,
-           #title = titre)
+Q <- qgraph(cor_matrix_reduite, layout = "spring", posCol = "blue", negCol = "NA",
+            nodeNames = colnames(cor_matrix_reduite), legend.cex = 0.2,
+            groups = group_matrix_reduite,
+            label.scale = FALSE,
+            minimum = 0.005, # Ne pas afficher les edges avec faible corrélation pour faciliter le chargement graphique
+            #repulsion = 100, # Augmenter la distance entre les noeuds pour améliorer la visualisation en "cluster"
+            legend.mode = "groupe",sampleSize = nrow(cor_matrix_reduite), alpha = 0.05, # Pour ne prendre en compte que les corrélations statistiquement significatifs, en tenant compte de l'inflation du risque alpha par les tests multiples via la méthode de Bonferroni.
+            palette = "pastel", vsize = 2)
+
+#labels=TRUE)
+#color = color,
+#title = titre)
 
 # save
 save(Q, file = output_file)
