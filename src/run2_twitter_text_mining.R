63bb536fb092f5fef9137c2900d983b716450967
ChristopheGauld <54821696+ChristopheGauld@users.noreply.github.com>
2020-11-21 11:08:15 +0100

    Update run2_twitter_text_mining.R

--- src/run2_twitter_text_mining.R
+++ src/run2_twitter_text_mining.R
@@ -30,18 +30,17 @@ tidy_twitter <- autis_tweets %>%
 
 # cleaning
 data("stop_words")
-stop_words = rbind (stop_words,"amp","sm","asd")
+stop_words = rbind (stop_words,"amp","sm","asd","autism","asd", "1", "2", "3", "4", "5", "6", "7", "8", "9", "covid", "2020", "behavioral","increased","found","identified","patients","reported","including","developing","examined","participants","suggest","compared","significantly","based","na","findings","related","results","children","significant","spectrum","study","control","provide","review","studies","effects","analysis","specific","age","data","behaviors","observed","potential","lower","included","scale")
 tidy_twitter2 <- tidy_twitter %>% 
             anti_join(stop_words)
 
 tidy_twitter3 <- tidy_twitter2[!str_detect(tidy_twitter2$word,"autis"),]
-tidy_twitter4 <- tidy_twitter3[!str_detect(tidy_twitter3$word, c("1","2","3","4","5","6","7","8","9")),]
 
 # count frequency of each word 
-tidy_twitter5 <- dplyr::count(tidy_twitter4, id, word, sort=TRUE)
+tidy_twitter4 <- dplyr::count(tidy_twitter3, id, word, sort=TRUE)
 
 # convert to a dtm 
-dtm_twitter <- cast_dtm(tidy_twitter5, id, word, n)
+dtm_twitter <- cast_dtm(tidy_twitter4, id, word, n)
 
 # save
 save(tidy_twitter5, dtm_twitter, autis_tweets, file = output_file)
