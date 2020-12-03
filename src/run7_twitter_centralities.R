91d79de1ea5bef8e3e19288a697e54de2434b89e
ChristopheGauld <54821696+ChristopheGauld@users.noreply.github.com>
2020-12-03 10:24:54 +0100

    Update run7_twitter_centralities.R

--- src/run7_twitter_centralities.R
+++ src/run7_twitter_centralities.R
@@ -9,16 +9,16 @@
 
 library(qgraph)
 # matrix ponderation 
-S <- getWmat(g,directed = FALSE)
+S <- getWmat(Q,directed = FALSE)
 # Measures of centrality
-deg <- centrality(g)$OutDegree
-bet <- centrality(r)$Betweenness
-clo <- centrality(r)$Closeness
+deg <- centrality(Q)$OutDegree
+bet <- centrality(Q)$Betweenness
+clo <- centrality(Q)$Closeness
 # centrality and clustering
-centralityPlot(S, labels = Labels, scale = c("z-scores", "raw", "raw0","relative"),
+centralityPlot(S, scale = c("z-scores", "raw", "raw0","relative"),
                include =c("Degree","Strength","OutDegree","InDegree","OutStrength",
                           "InStrength"), theme_bw = TRUE, print = TRUE, verbose = TRUE,
-               standardized, relative, weighted = TRUE,signed = TRUE,
+               weighted = TRUE,signed = TRUE,
                orderBy = "default", decreasing = FALSE)
 clusteringPlot(S, scale = c("z-scores", "raw", "raw0","relative"), labels , signed = FALSE, theme_bw = TRUE, print = TRUE,verbose = TRUE)
 
