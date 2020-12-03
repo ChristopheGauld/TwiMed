1ac34657be5a86c062d8f85b4dfc343bba280cdd
ChristopheGauld <54821696+ChristopheGauld@users.noreply.github.com>
2020-12-03 10:12:23 +0100

    Update run4_twitter_qgraph.R

--- src/run4_twitter_qgraph.R
+++ src/run4_twitter_qgraph.R
@@ -14,10 +14,18 @@ output_file1 <- "../fig/twitter_graph_50.pdf"
 output_file2 <- "../data/twitter_qgraph_50.Rdata"
 
 library(qgraph)
+library(NbClust)
 
 # load the pubmed tidy object and the terms order by group after topic modeling
 load(input_file1)
 load(input_file2)
+
+# Best numbers of Clusters
+res<-NbClust(cor_matrix_reduite, distance = "euclidean", min.nc=2, max.nc=8, 
+             method = "complete", index = "ch")
+res$All.index
+res$Best.nc
+res$Best.partition
 
 # Selection of the 50 most frequent words to plot the graph
 nNode <- 50
@@ -29,24 +37,22 @@ group1_reduit <- which(colnames(cor_matrix_reduite) %in% group[[1]])
 group2_reduit <- which(colnames(cor_matrix_reduite) %in% group[[2]]) 
 group3_reduit <- which(colnames(cor_matrix_reduite) %in% group[[3]]) 
 group4_reduit <- which(colnames(cor_matrix_reduite) %in% group[[4]]) 
-group5_reduit <- which(colnames(cor_matrix_reduite) %in% group[[5]]) 
-group6_reduit <- which(colnames(cor_matrix_reduite) %in% group[[6]]) 
-group7_reduit <- which(colnames(cor_matrix_reduite) %in% group[[7]]) 
-group_matrix_reduite <- list(group1_reduit,group2_reduit,group3_reduit,group4_reduit,group5_reduit,group6_reduit,group7_reduit)
+group_matrix_reduite <- list(group1_reduit,group2_reduit,group3_reduit,group4_reduit)
 
 # create a qgraph object
 pdf(file = output_file1)
-Q <- qgraph(cor_matrix_reduite, layout = "spring", posCol = "red", negCol = "NA",
+Q <- qgraph(cor_matrix_reduite, layout = "groups", posCol = "blue", negCol = "NA",
             nodeNames = colnames(cor_matrix_reduite), legend.cex = 0.2,
             groups = group_matrix_reduite,
             label.scale = TRUE,
+            curveAll = TRUE,
             labels = TRUE,
-            minimum = 0.08, # 0.15 quand 500 noeuds
-            repulsion = 1., # Augmenter la distance entre les noeuds pour améliorer la visualisation en "cluster"
-            legend.mode = "style2", # groupe quand 500 noeuds
+            minimum = 0.08, 
+            repulsion = 1., 
+            legend.mode = "style2",
             nodeNames= TRUE,
             sampleSize = nrow(cor_matrix_reduite),
-            alpha = 0.05, # Pour ne prendre en compte que les corrélations statistiquement significatifs, en tenant compte de l'inflation du risque alpha par les tests multiples via la méthode de Bonferroni.
+            alpha = 0.05, #inflation du risque alpha par les tests multiples via la méthode de Bonferroni.
             palette = "pastel",
             vsize = 4,
             cut = 0.3)
@@ -77,17 +83,14 @@ group1_reduit <- which(colnames(cor_matrix_reduite) %in% group[[1]])
 group2_reduit <- which(colnames(cor_matrix_reduite) %in% group[[2]]) 
 group3_reduit <- which(colnames(cor_matrix_reduite) %in% group[[3]]) 
 group4_reduit <- which(colnames(cor_matrix_reduite) %in% group[[4]]) 
-group5_reduit <- which(colnames(cor_matrix_reduite) %in% group[[5]]) 
-group6_reduit <- which(colnames(cor_matrix_reduite) %in% group[[6]]) 
-group7_reduit <- which(colnames(cor_matrix_reduite) %in% group[[7]]) 
-group_matrix_reduite <- list(group1_reduit,group2_reduit,group3_reduit,group4_reduit,group5_reduit,group6_reduit,group7_reduit)
+group_matrix_reduite <- list(group1_reduit,group2_reduit,group3_reduit,group4_reduit)
 
 # create a qgraph object
 pdf(file = output_file1, width=14,height=14)
 Q <- qgraph(cor_matrix_reduite, layout = "groups", posCol = "blue", negCol = "NA",
             nodeNames = colnames(cor_matrix_reduite), legend.cex = 0.2,
             vsize = 3,
-            #curveAll = TRUE,
+            curveAll = TRUE,
             esize = 3,
             label.cex = 2,
             groups = group_matrix_reduite,
