08b95308a9f01a85d8227d4a9a6ffdf5ce05ae61
Guillaume Dumas <deep@introspection.eu>
2021-01-23 15:40:55 -0500

    Correct ggplot print + clarify file naming

--- src/run6_pubmed_matrix_to_bootstrap.R
+++ src/run6_pubmed_matrix_to_bootstrap.R
@@ -18,8 +18,12 @@ source("load_function_violin.R")
 
 #### Use run_pubmed to obtain the matrix_pubmed
 input_file <- "../data/pubmed_tdm.Rdata"
-output_file <- "../fig/fig1b.pdf"
+output_file1 <- "../fig/pubmed_bs.pdf"
+output_file2 <- "../data/pubmed_bs.Rdata"
 load(input_file)
+
+# Number of bootstraps
+n_bs <- 1000
 
 # Take only the 15 most frequent words (repeat)
 words_pubmed15 = tidy.pubmed4$word[1:15]
@@ -34,19 +38,20 @@ occurence_random_words <- function(data, indices, word) {
 }
 
 # initialisation d'un tableau de résultat vide
-results=matrix(NA,1000,15)
+results=matrix(NA,n_bs,15)
 results=as.data.frame(results)
 # Pour chacun des 15 mots, on calcule les 1000 valeurs de bootsrap et on les mets dans une colonne du tableau de résultats
 
 for (i in 1:15) {
-  result <- boot(data=ab.pubmed, statistic=occurence_random_words, R=1000, word=words_pubmed15[i])
-  results[,i]=result$t # On remplit une colonne du dataframe des résultats avec les 1000 valeurs de bootsrap
+  result <- boot(data=ab.pubmed, statistic=occurence_random_words, R=n_bs, word=words_pubmed15[i])
+  results[,i]=result$t # On remplit une colonne du dataframe des résultats avec les valeurs de bootsrap
   names(results)[i]=words_pubmed15[i] # On donne le mot en nom de variable du dataframe
   print(names(results)[i])
 }
+save(results, file = output_file2)
 
 ## And plot the data
-source("/geom_flat_violin.R") # cf run_twitter_03b
+#source("load_function_violin.R") # cf run_twitter_03b
 
 ## Select  columns + reorder
 inputData_pubmed <- select(results,colnames(results))
@@ -61,44 +66,39 @@ plotData_pubmed <- gather(inputData_pubmed,
 
 ## Plot
 p <- ggplot(plotData_pubmed, aes(x = condition, y = value, fill = condition, color = condition)) +
-    ggtitle("Bootstrap of the unique words count find in PubMed") +
-    ylab("Top fifteen words") +
-    xlab("Assigned Probability Score") +
-    theme_cowplot() +
-    scale_shape_identity() +
-    theme(legend.position = "none",
-          plot.title = element_text(size = 20),
-          axis.title = element_text(size = 15),
-          axis.text = element_text(size = 12),
-          axis.text.x = element_text(angle = 0, 
-                                     hjust = 0,
-                                     vjust = 0)) +
-    scale_color_igv() +
-    scale_fill_igv() +
-    geom_point(position = position_jitter(0.2), 
-               size = 0, 
-               alpha = 1, 
-               aes(shape = 16)) +
-    geom_flat_violin(position = position_nudge(x = 0.25, y = 0),
-                     adjust = 2,
-                     alpha = 0.6, 
-                     trim = TRUE, 
-                     scale = "width") +
-    geom_boxplot(aes(x = as.numeric(condition) + 0.25, y = value), 
-                 notch = TRUE, 
-                 width = 0.1, 
-                 varwidth = FALSE, 
-                 outlier.shape = NA, 
-                 alpha = 0.3, 
-                 colour = "black", 
-                 show.legend = FALSE) +
-    coord_flip()
+            ggtitle("Bootstrap of the unique words count find in PubMed") +
+            ylab("Top fifteen words") +
+            xlab("Assigned Probability Score") +
+            theme_cowplot() +
+            scale_shape_identity() +
+            theme(legend.position = "none",
+                  plot.title = element_text(size = 20),
+                  axis.title = element_text(size = 15),
+                  axis.text = element_text(size = 12),
+                  axis.text.x = element_text(angle = 0, 
+                                             hjust = 0,
+                                             vjust = 0)) +
+            scale_color_igv() +
+            scale_fill_igv() +
+            geom_point(position = position_jitter(0.2), 
+                       size = 0, 
+                       alpha = 1, 
+                       aes(shape = 16)) +
+            geom_flat_violin(position = position_nudge(x = 0.25, y = 0),
+                             adjust = 2,
+                             alpha = 0.6, 
+                             trim = TRUE, 
+                             scale = "width") +
+            geom_boxplot(aes(x = as.numeric(condition) + 0.25, y = value), 
+                         notch = TRUE, 
+                         width = 0.1, 
+                         varwidth = FALSE, 
+                         outlier.shape = NA, 
+                         alpha = 0.3, 
+                         colour = "black", 
+                         show.legend = FALSE) +
+            coord_flip()
 
-
-
-
-
-# plot in a pdf file
-pdf(file = output_file, width=14, height=14)
-p
+pdf(file = output_file1, width=14, height=14)
+print(p)
 dev.off()
--- src/run6_twitter_matrix_to_bootstrap.R
+++ src/run6_twitter_matrix_to_bootstrap.R
@@ -18,8 +18,12 @@ source("load_function_violin.R")
 
 #### load the tidy objet of twitter words
 input_file <- "../data/twitter_tdm.Rdata"
-output_file <- "../fig/fig1a.pdf"
+output_file1 <- "../fig/twitter_bs.pdf"
+output_file2 <- "../data/twitter_bs.Rdata"
 load(input_file)
+
+# Number of bootstraps
+n_bs <- 1000
 
 # Take only the 15 most frequent words (repeat)
 words_twitter15 <- tidy_twitter4$word[1:15]
@@ -34,19 +38,19 @@ occurence_random_tweet <- function(data, indices, word) {
 }
 
 # initialisation d'un tableau de résultat vide
-results2 <- matrix(NA,1000,15)
-results2 <- as.data.frame(results)
-# For each of the 15 words, we calculate the 1000 values of bootstrap and put them in a column of the results table
+results2 <- matrix(NA,n_bs,15)
+results2 <- as.data.frame(results2)
+# For each of the 15 words, we calculate the values of bootstrap and put them in a column of the results table
 for (i in 1:15) {
-  result2 <- boot(data=autis_tweets, statistic=occurence_random_tweet, R=1000, word=words_twitter15[i])
-  results2[,i] <- result2$t # We fill a column of the dataframe of the results with the 1000 values of bootsrap
+  result2 <- boot(data=autis_tweets, statistic=occurence_random_tweet, R=n_bs, word=words_twitter15[i])
+  results2[,i] <- result2$t # We fill a column of the dataframe of the results with the values of bootsrap
   names(results2)[i] <- words_twitter15[i] 
   print(names(results2)[i])
 }
-
+save(results2, file = output_file2)
 
 ## And plot the data
-source("../src/run5_twitter_load_function_violin.R") 
+#source("load_function_violin.R") 
 
 inputData_twitter <- select(results2,colnames(results2))
 
@@ -60,41 +64,39 @@ plotData_twit <- gather(inputData_twitter,
 
 ## Plot
 p <- ggplot(plotData_twit, aes(x = condition, y = value, fill = condition, color = condition)) +
-     ggtitle("Bootstrap of the unique words count find in Twitter") +
-     ylab("Top fifteen words") +
-     xlab("Assigned Probability Score") +
-     theme_cowplot() +
-     scale_shape_identity() +
-     theme(legend.position = "none",
-           plot.title = element_text(size = 20),
-           axis.title = element_text(size = 15),
-           axis.text = element_text(size = 12),
-           axis.text.x = element_text(angle = 0, 
-                                      hjust = 0,
-                                      vjust = 0)) +
-     scale_color_igv() +
-     scale_fill_igv() +
-     geom_point(position = position_jitter(0.2), 
-                size = 0, 
-                alpha = 1, 
-                aes(shape = 16)) +
-     geom_flat_violin(position = position_nudge(x = 0.25, y = 0),
-                      adjust = 2,
-                      alpha = 0.6, 
-                      trim = TRUE, 
-                      scale = "width") +
-     geom_boxplot(aes(x = as.numeric(condition) + 0.25, y = value), 
-                  notch = TRUE, 
-                  width = 0.1, 
-                  varwidth = FALSE, 
-                  outlier.shape = NA, 
-                  alpha = 0.3, 
-                  colour = "black", 
-                  show.legend = FALSE) +
-     coord_flip()
+            ggtitle("Bootstrap of the unique words count find in Twitter") +
+            ylab("Top fifteen words") +
+            xlab("Assigned Probability Score") +
+            theme_cowplot() +
+            scale_shape_identity() +
+            theme(legend.position = "none",
+                  plot.title = element_text(size = 20),
+                  axis.title = element_text(size = 15),
+                  axis.text = element_text(size = 12),
+                  axis.text.x = element_text(angle = 0, 
+                                             hjust = 0,
+                                             vjust = 0)) +
+            scale_color_igv() +
+            scale_fill_igv() +
+            geom_point(position = position_jitter(0.2), 
+                       size = 0, 
+                       alpha = 1, 
+                       aes(shape = 16)) +
+            geom_flat_violin(position = position_nudge(x = 0.25, y = 0),
+                             adjust = 2,
+                             alpha = 0.6, 
+                             trim = TRUE, 
+                             scale = "width") +
+            geom_boxplot(aes(x = as.numeric(condition) + 0.25, y = value), 
+                         notch = TRUE, 
+                         width = 0.1, 
+                         varwidth = FALSE, 
+                         outlier.shape = NA, 
+                         alpha = 0.3, 
+                         colour = "black", 
+                         show.legend = FALSE) + 
+            coord_flip()
 
-
-# plot in a pdf file
-pdf(file = output_file, width=14, height=14)
-p
+pdf(file = output_file1, width=14, height=14)
+print(p)
 dev.off()
